08d85f71e4f9adc7eb203dc203787c31f8794ea7
tamim <tamimarefinanik@gmail.com>
2023-01-25 07:57:18 +0600

    adding temp inmemory db and repository

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,37 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext dbContext;
+        public PlatformRepo(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+
+            dbContext.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return dbContext.Platforms.ToList();
+        }
+
+        public Platform GetPlatformById(int id)
+        {
+            return dbContext.Platforms.FirstOrDefault(p => p.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (dbContext.SaveChanges() >= 0);
+        }
+    }
+}
